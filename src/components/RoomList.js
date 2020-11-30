e55818efa6a99d478a08170041ee8ef51f1ba3fc
evangel <evangeldaping@gmail.com>
2020-11-29 18:42:20 -0800

    RoomList

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import Rooms from '../pages/Rooms'
+import Room from './Room'
 
-export default function RoomList() {
+export default function RoomList({rooms}) {
+    if(rooms.length === 0){
+        return (
+            <div className="empty-search">
+                <h3>unfortunately no rooms matched your search parameters</h3>
+            </div>
+        )
+    }
+
     return (
-        <div>
-            hello from rooms list
-        </div>
+        <section className="roomslist">
+            <div className="roomslist-center">
+                {
+                    rooms.map(item => {
+                        return <Room key={item.d} room={item} />
+                    })
+                }
+            </div>
+        </section>
     )
 }
